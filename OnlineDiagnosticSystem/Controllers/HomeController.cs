ea3361bf25a3b8545ad3c31b9bea5b7597d21704
Mohammad Ishaq <info@ishaq.io>
2020-04-02 14:19:19 -0500

    Add project files.

---
+++ OnlineDiagnosticSystem/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using DatabaseLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineDiagnosticSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private OnlineDiagnosticLabSystemDbEntities db = new OnlineDiagnosticLabSystemDbEntities();
+        public ActionResult Index()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            return View();
+        }
+        public ActionResult StartTemplate()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            if(string.IsNullOrEmpty(email) && string.IsNullOrEmpty(password))
+            {
+                return View("Login");
+            }
+            var user = db.UserTables.Where(u => u.Email == email && u.Password == password && u.isVerified == true).FirstOrDefault();
+            if (user != null)
+            {
+                Session["UserID"] = user.UserID;
+                Session["UserTypeID"] = user.UserTypeID;
+                Session["UserName"] = user.UserName;
+                Session["Password"] = user.Password;
+                Session["Email"] = user.Email;
+                Session["ContactNo"] = user.ContactNo;
+                Session["Description"] = user.Description;
+                Session["isVerified"] = user.isVerified;
+                return View("Index");
+
+            }
+            Logout();
+            return View("Login");
+
+        }
+
+        public void Logout()
+        {
+            Session["UserID"] = string.Empty;
+            Session["UserTypeID"] = string.Empty;
+            Session["UserName"] = string.Empty;
+            Session["Password"] = string.Empty;
+            Session["Email"] = string.Empty;
+            Session["ContactNo"] = string.Empty;
+            Session["Description"] = string.Empty;
+            Session["isVerified"] = string.Empty;
+          
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
