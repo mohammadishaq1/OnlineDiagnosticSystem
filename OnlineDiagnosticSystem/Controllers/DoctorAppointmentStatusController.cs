02f4c1282f266aeb32515cb6741c4d6625da7f26
Mohammad Ishaq <info@ishaq.io>
2020-04-08 15:49:42 -0500

    current appointment

---
+++ OnlineDiagnosticSystem/Controllers/DoctorAppointmentStatusController.cs
@@ -0,0 +1,51 @@
+﻿using DatabaseLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineDiagnosticSystem.Controllers
+{
+    public class DoctorAppointmentStatusController : Controller
+    {
+        private OnlineDiagnosticLabSystemDbEntities db = new OnlineDiagnosticLabSystemDbEntities();
+        // GET: DoctorAppointmentStatus
+        public ActionResult PendingAppoint()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+           
+            var patient = (PatientTable)Session["Patient"];
+            var pendingappointment = db.DoctorAppointTables.Where(d => d.PatientID == patient.PatientID && d.IsChecked == false && d.IsFeeSubmit == false && d.DoctorComment.Trim().Length == 0);
+            return View(pendingappointment);
+        }
+
+
+        public ActionResult CurrentAppoint()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            var patient = (PatientTable)Session["Patient"];
+            var currentappointment = db.DoctorAppointTables.Where(d => d.PatientID == patient.PatientID && d.IsChecked == false && d.IsFeeSubmit == true && d.DoctorComment.Trim().Length == 0);
+            return View(currentappointment);
+        }
+
+        public ActionResult CompleteAppoint()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            var patient = (PatientTable)Session["Patient"];
+            var completeappointment = db.DoctorAppointTables.Where(d => d.PatientID == patient.PatientID && d.IsChecked == true && d.IsFeeSubmit == true && d.DoctorComment.Trim().Length > 0);
+            return View(completeappointment);
+        }
+    }
+}
