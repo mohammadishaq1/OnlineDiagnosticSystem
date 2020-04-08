8e483cae5506544ef1eec82f1c7fcefbe4b351a4
Mohammad Ishaq <info@ishaq.io>
2020-04-08 14:52:06 -0500

    make appointment

---
+++ OnlineDiagnosticSystem/Controllers/PatientAppointController.cs
@@ -0,0 +1,106 @@
+﻿using DatabaseLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineDiagnosticSystem.Controllers
+{
+    public class PatientAppointController : Controller
+    {
+        private OnlineDiagnosticLabSystemDbEntities db = new OnlineDiagnosticLabSystemDbEntities();
+        // GET: PatientAppoint
+        public ActionResult GetAllDoctors()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            var doclist = db.DoctorTables.Where(d => d.UserTable.isVerified == true);
+            return View(doclist);
+        }
+
+        public ActionResult GetAllLabs()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            var lablist = db.LabTables.Where(d => d.UserTable.isVerified == true);
+            return View(lablist);
+        }
+
+
+        public ActionResult DocAppointment(int? id)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            Session["docid"] = id;
+            ViewBag.DoctorTimeSlotID = new SelectList(db.DoctorTimeSlotTables.Where(d => d.DoctorID == id && d.IsActive == true), "DoctorTimeSlotID", "Name","0");
+            ViewBag.Doctor = db.DoctorTables.Find(id);
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DocAppointment(DoctorAppointTable appointment)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            appointment.DoctorComment = string.Empty;
+            appointment.IsChecked = false;
+            appointment.IsFeeSubmit = false;
+            var patient = (PatientTable)Session["Patient"];
+            appointment.PatientID = patient.PatientID;
+            appointment.DoctorID = Convert.ToInt32(Convert.ToString(Session["docid"]));
+            if (ModelState.IsValid)
+            {
+                var checktransectionno = db.DoctorAppointTables.Where(c => c.TransactionNo == appointment.TransactionNo).FirstOrDefault();
+                if (checktransectionno == null)
+                {
+                    var find = db.DoctorAppointTables.Where(p => p.DoctorTimeSlotID == appointment.DoctorTimeSlotID && p.DoctorID == appointment.DoctorID && p.AppointDate == appointment.AppointDate).FirstOrDefault();
+                    if (find == null)
+                    {
+                        db.DoctorAppointTables.Add(appointment);
+                        db.SaveChanges();
+                        return RedirectToAction("DoctorPendingAppoint");
+                    }
+                    else
+                    {
+                        ViewBag.Message = "This time slot is already assigned";
+                    }
+                }
+                else
+                {
+                    ViewBag.Message = "Transaction no is already used";
+                }
+            }
+            ViewBag.DoctorTimeSlotID = new SelectList(db.DoctorTimeSlotTables.Where(d => d.DoctorID == appointment.DoctorID && d.IsActive == true), "DoctorTimeSlotID", "Name", "0");
+            return View();
+        }
+        public ActionResult DoctorProfile(int? id)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            var doc= db.DoctorTables.Find(id);
+            return View(doc);
+        }
+
+        public ActionResult DoctorPendingAppoint(int? id)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            var userid = Convert.ToInt32(Convert.ToString(Session["UserID"]));
+            var patientid = db.PatientTables.Where(p => p.UserID == userid).FirstOrDefault().PatientID;
+            var appointmentlist = db.DoctorAppointTables.Where(p => p.PatientID == patientid && p.IsFeeSubmit == false && p.IsChecked == false);
+            return View(appointmentlist);
+        }
+    }
+}
