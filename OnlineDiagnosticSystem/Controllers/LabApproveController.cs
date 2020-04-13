db69c46c415535cbf4cccf8796bd9c37506f952b
Mohammad Ishaq <info@ishaq.io>
2020-04-13 08:59:26 -0500

    Reports

--- OnlineDiagnosticSystem/Controllers/LabApproveController.cs
+++ OnlineDiagnosticSystem/Controllers/LabApproveController.cs
@@ -67,7 +67,7 @@ namespace OnlineDiagnosticSystem.Controllers
             }
 
             var appoint = db.LabAppointTables.Find(id);
-            ViewBag.LabTimeSlotID = new SelectList(db.LabTimeSlotTables.Where(d => d.LabID == appoint.LabID), "LabTimeSlotID", "Name", appoint.LabTimeSlotID); 
+            ViewBag.LabTimeSlotID = new SelectList(db.LabTimeSlotTables.Where(d => d.LabID == appoint.LabID), "LabTimeSlotID", "Name", appoint.LabTimeSlotID);
             return View(appoint);
         }
         [HttpPost]
@@ -105,7 +105,7 @@ namespace OnlineDiagnosticSystem.Controllers
                     LabAppointID = appoint.LabAppointID,
                     LabTestDetailID = item.LabTestDetailID,
                     MaxValue = item.MaxValue,
-                    MinValue=item.MinValue,
+                    MinValue = item.MinValue,
                     PatientValue = 0
                 };
                 detaillist.Add(details);
@@ -113,7 +113,82 @@ namespace OnlineDiagnosticSystem.Controllers
             ViewBag.TestName = appoint.LabTestTable.Name;
 
             return View(detaillist);
-            
+
+        }
+
+        [HttpPost]
+        public ActionResult ProcessAppointment(FormCollection collection)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            string[] LabTestDetailID = collection["item.LabTestDetailID"].Split(',');
+            string[] LabAppointID = collection["item.LabAppointID"].Split(',');
+            string[] DetailName = collection["item.DetailName"].Split(',');
+            string[] MinValue = collection["item.MinValue"].Split(',');
+            string[] MaxValue = collection["item.MaxValue"].Split(',');
+            string[] PatientValue = collection["item.PatientValue"].Split(',');
+            List<PatientAppointmentMV> detaillist = new List<PatientAppointmentMV>();
+
+            bool issubmit = false;
+            for (int i = 0; i < LabTestDetailID.Length; i++)
+            {
+                var details = new PatientAppointmentMV()
+                {
+                    DetailName = DetailName[i],
+                    LabAppointID = Convert.ToInt32(LabAppointID[i]),
+                    LabTestDetailID = Convert.ToInt32(LabTestDetailID[i]),
+                    MaxValue = Convert.ToInt32(MaxValue[i]),
+                    MinValue = Convert.ToInt32(MinValue[i]),
+                    PatientValue = Convert.ToInt32(PatientValue[i])
+                };
+                if (details.PatientValue > 0)
+                {
+                    issubmit = true;
+                }
+                detaillist.Add(details);
+            }
+            if (issubmit == true)
+            {
+
+
+                foreach (var item in detaillist)
+                {
+                    var pdetails = new PatientTestDetailTable()
+                    {
+                        LabAppointID = item.LabAppointID,
+                        LabTestDetailID = item.LabTestDetailID,
+                        PatientValue = item.PatientValue
+                    };
+                    db.PatientTestDetailTables.Add(pdetails);
+                    db.SaveChanges();
+                }
+                int appointid = Convert.ToInt32(LabAppointID[0]);
+                var appoint = db.LabAppointTables.Find(appointid);
+                appoint.IsComplete = true;
+                db.Entry(appoint).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("PendingAppoint");
+            }
+            else
+            {
+                ViewBag.Message = "Please Enter Patient Test Details";
+            }
+            return View(detaillist);
+
+        }
+
+        public ActionResult ViewDetails(int? id)
+        {
+
+            ViewBag.TestName = db.LabAppointTables.Find(id).LabTestTable.Name;
+            ViewBag.Lab = db.LabAppointTables.Find(id).LabTable.Name;
+            ViewBag.Patient = db.LabAppointTables.Find(id).PatientTable.Name;
+            ViewBag.AppointmentNo = db.LabAppointTables.Find(id).TransactionNo;
+            ViewBag.PatientPhoto = db.LabAppointTables.Find(id).PatientTable.Photo;
+
+            return View(db.PatientTestDetailTables.Where(p => p.LabAppointID == id).ToList());
         }
     }
 }
